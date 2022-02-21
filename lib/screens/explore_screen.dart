9bcbd4543cfb84daa5531d75e1e77e769e945a08
udohahanor <udohahanor@gmail.com>
2022-02-21 18:39:35 +0100

    explore page

--- lib/screens/explore_screen.dart
+++ lib/screens/explore_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:skyhigh/widgets/widgets.dart';
 
 class ExploreScreen extends StatelessWidget {
   const ExploreScreen({Key? key}) : super(key: key);
@@ -6,29 +7,60 @@ class ExploreScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Container(
-        color: Colors.blueGrey[900],
-        child: ListView(
-          scrollDirection: Axis.vertical,
-          children: [
-            Container(
-              width: double.infinity,
-              height: 400,
-              decoration: const BoxDecoration(
-                color: Colors.blue,
-                image: DecorationImage(
-                  image: AssetImage('assets/images/deco-img.jpg'),
-                  fit: BoxFit.cover,
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        body: Container(
+          color: Colors.transparent,
+          child: ListView(
+            scrollDirection: Axis.vertical,
+            children: [
+              const NavBar(),
+              const SizedBox(height: 20.0),
+              Container(
+                width: double.infinity,
+                height: 500,
+                decoration: const BoxDecoration(
+                  color: Colors.transparent,
+                  image: DecorationImage(
+                    image: AssetImage('assets/images/deco-img.jpg'),
+                    fit: BoxFit.cover,
+                  ),
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(30.0),
+                    topRight: Radius.circular(30.0),
+                    bottomLeft: Radius.circular(30.0),
+                    bottomRight: Radius.circular(30.0),
+                  ),
                 ),
-                borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(30.0),
-                  topRight: Radius.circular(30.0),
-                  bottomLeft: Radius.circular(30.0),
-                  bottomRight: Radius.circular(30.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    const Text(
+                      'Tokyo, Japan',
+                      style: TextStyle(
+                        fontFamily: 'Poppins',
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                        fontSize: 40.0,
+                      ),
+                    ),
+                    const SizedBox(height: 120.0),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 50.0),
+                      child: Container(
+                        width: double.infinity,
+                        height: 40.0,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(40.0),
+                          color: Colors.white.withOpacity(0.4),
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
--- lib/widgets/navbar.dart
+++ lib/widgets/navbar.dart
@@ -5,17 +5,77 @@ class NavBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: double.infinity,
-      height: 200.0,
-      color: Colors.blue,
-      child: Row(
-        children: [
-          Image.asset(
-            'assets/logos/skyhigh-logos.png',
-            height: 100.0,
+    return SafeArea(
+      child: Container(
+        width: double.infinity,
+        height: 30.0,
+        color: Colors.transparent,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 10.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Image.asset(
+                'assets/logos/skyhigh-logos.png',
+                height: 15.0,
+                color: Colors.black,
+              ),
+              const Spacer(),
+              Row(
+                children: const [
+                  Text(
+                    'Package',
+                    style: TextStyle(
+                      fontFamily: 'Poppins',
+                      color: Colors.black,
+                      fontSize: 13.0,
+                    ),
+                  ),
+                  SizedBox(width: 50.0),
+                  Text(
+                    'Destination',
+                    style: TextStyle(
+                      fontFamily: 'Poppins',
+                      color: Colors.black,
+                      fontSize: 13.0,
+                    ),
+                  ),
+                  SizedBox(width: 50.0),
+                  Text(
+                    'Contact',
+                    style: TextStyle(
+                      fontFamily: 'Poppins',
+                      color: Colors.black,
+                      fontSize: 13.0,
+                    ),
+                  ),
+                ],
+              ),
+              const Spacer(),
+              Row(
+                children: const [
+                  Icon(
+                    Icons.location_on_outlined,
+                    size: 15.0,
+                    color: Colors.black,
+                  ),
+                  SizedBox(width: 20.0),
+                  Icon(
+                    Icons.account_circle_outlined,
+                    size: 15.0,
+                    color: Colors.black,
+                  ),
+                  SizedBox(width: 20.0),
+                  Icon(
+                    Icons.menu_outlined,
+                    size: 15.0,
+                    color: Colors.black,
+                  ),
+                ],
+              ),
+            ],
           ),
-        ],
+        ),
       ),
     );
   }
