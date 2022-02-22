3483f976301f40c840c4e0a0e5cedf160afd94ed
udohahanor <udohahanor@gmail.com>
2022-02-22 17:58:42 +0100

    responsive script

--- lib/screens/explore_screen.dart
+++ lib/screens/explore_screen.dart
@@ -9,57 +9,61 @@ class ExploreScreen extends StatelessWidget {
     return SafeArea(
       child: Scaffold(
         backgroundColor: Colors.transparent,
-        body: Container(
-          color: Colors.transparent,
-          child: ListView(
-            scrollDirection: Axis.vertical,
-            children: [
-              const NavBar(),
-              const SizedBox(height: 20.0),
-              Container(
-                width: double.infinity,
-                height: 500,
-                decoration: const BoxDecoration(
-                  color: Colors.transparent,
-                  image: DecorationImage(
-                    image: AssetImage('assets/images/deco-img.jpg'),
-                    fit: BoxFit.cover,
-                  ),
-                  borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(30.0),
-                    topRight: Radius.circular(30.0),
-                    bottomLeft: Radius.circular(30.0),
-                    bottomRight: Radius.circular(30.0),
-                  ),
-                ),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: [
-                    const Text(
-                      'Tokyo, Japan',
-                      style: TextStyle(
-                        fontFamily: 'Poppins',
-                        fontWeight: FontWeight.bold,
-                        color: Colors.white,
-                        fontSize: 40.0,
-                      ),
+        body: Padding(
+          padding: const EdgeInsets.all(2.0),
+          child: Container(
+            color: Colors.transparent,
+            child: ListView(
+              scrollDirection: Axis.vertical,
+              children: [
+                const NavBar(),
+                const SizedBox(height: 20.0),
+                Container(
+                  width: double.infinity,
+                  height: 500,
+                  decoration: const BoxDecoration(
+                    color: Colors.transparent,
+                    image: DecorationImage(
+                      image: AssetImage('assets/images/deco-img.jpg'),
+                      fit: BoxFit.cover,
                     ),
-                    const SizedBox(height: 120.0),
-                    Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 50.0),
-                      child: Container(
-                        width: double.infinity,
-                        height: 40.0,
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(40.0),
-                          color: Colors.white.withOpacity(0.4),
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(25.0),
+                      topRight: Radius.circular(25.0),
+                      bottomLeft: Radius.circular(25.0),
+                      bottomRight: Radius.circular(25.0),
+                    ),
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const SizedBox(height: 100.0),
+                      const Text(
+                        'Tokyo, Japan',
+                        style: TextStyle(
+                          fontFamily: 'Poppins',
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                          fontSize: 35.0,
                         ),
                       ),
-                    ),
-                  ],
+                      const SizedBox(height: 260.0),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 50.0),
+                        child: Container(
+                          width: 800.0,
+                          height: 40.0,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(40.0),
+                            color: Colors.white.withOpacity(0.3),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
